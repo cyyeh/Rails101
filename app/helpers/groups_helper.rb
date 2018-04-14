5325b8b4169ba284fc42e2e4cc5827f3a80a855b
cyyeh <chihyu.jimmy.yeh@gmail.com>
2018-04-14 15:10:46 +0800

    add custom view helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -6,4 +6,8 @@ module GroupsHelper
   def is_current_user_belongs_to?(group)
     current_user && current_user.is_member_of?(group)
   end
+
+  def render_group_description(group)
+    simple_format(group.description)
+  end
 end
